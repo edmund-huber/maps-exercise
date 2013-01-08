1d7662cc90bdd61bb4a8eed7539acc48ad05ef8c
Edmund Huber <edmund@nang.(none)>
2013-01-07 20:21:20 -0800

    heroku: bind to 0.0.0.0

--- web.py
+++ web.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     import os
     from bottle import run
     port = int(os.environ.get('PORT', 8000))
-    run(host='localhost', port=port)
+    run(host='0.0.0.0', port=port)
 
