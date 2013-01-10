fa9b9a5efca80bc1310218dd7f7fe39d589944b6
Edmund Huber <edmund@nang.(none)>
2013-01-09 17:50:25 -0800

    cleanup

--- static/home.js
+++ static/home.js
@@ -1,72 +1,69 @@
 (function() {
 
-    var mapOptions = {
+    Places = window.Places || {};
+
+    Places.map = new google.maps.Map($("#map-canvas")[0], {
         center: new google.maps.LatLng(-34.397, 150.644),
         zoom: 8,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    });
 
-    var map = new google.maps.Map($("#map-canvas")[0] , mapOptions);
-    var geocoder = new google.maps.Geocoder();
+    Places.geocoder = new google.maps.Geocoder();
 
+    // Show (up to 100 of) the places in the maps bounds.
     var markers = {};
-    var showPlacesInMap = function() {
-        if (map.getBounds()) {
-            $.ajax('/api/all-in-bounds/' + map.getBounds().toUrlValue())
-                .fail(function() {
-                    console.log('all-in-bounds lookup failed');
-                })
-                .done(function(d) {
-                    $.each(d.results, function(_, i) {
-                        if (!markers.hasOwnProperty(i.id)) {
-                            var marker = new google.maps.Marker({position: new google.maps.LatLng(i.latitude, i.longitude), title: i.name});
-                            marker.setMap(map);
-                            markers[i.id] = 1;
-                            var makeInfoWindow = function() {
-                                var updateForm = $('<form/>', {class: 'marker-update-form'});
-                                updateForm.append($('<input/>', {type: 'hidden', name: 'id', value: i.id}));
-                                updateForm.append($('<input/>', {type: 'text', name: 'name', value: i.name}));
-                                updateForm.append($('<input/>', {type: 'text', name: 'longitude', value: i.longitude}));
-                                updateForm.append($('<input/>', {type: 'text', name: 'latitude', value: i.latitude}));
-                                updateForm.append($('<input/>', {type: 'text', name: 'address', value: i.address}));
-                                updateForm.append($('<input/>', {type: 'submit', value: 'Update'}));
-                                var deleteLink = $('<span/>', {id: 'delete-link', class: 'fake-link', text: 'delete'});
-                                updateForm.append(deleteLink);
-                                updateForm.submit(function(e) {
-                                    e.preventDefault();                                    
-                                    $.ajax({type: 'POST', url: '/api/update', data: $(e.target).formToJSON()})
-                                    infoWindow.close();
-                                });
-                                deleteLink.click(function() {
-                                    $.ajax({type: 'POST', url: '/api/delete', data: {id: i.id}});
-                                    infoWindow.close();
-                                });
-                                var infoWindow = new google.maps.InfoWindow({content: updateForm[0]});
-                                infoWindow.open(map, marker);
-                            };
-                            google.maps.event.addListener(marker, 'click', makeInfoWindow);
-                        }
-                    });
-                });
+    Places.showPlacesInMap = function() {
+
+        // The map may not have fully loaded yet.
+        if (!Places.map.getBounds()) {
+            return;
         }
+
+        $.ajax('/api/all-in-bounds/' + Places.map.getBounds().toUrlValue())
+            .fail(function() {
+                console.log('all-in-bounds lookup failed');
+            })
+            .done(function(d) {
+                // Add a marker for this place if there isn't one already.
+                $.each(d.results, function(_, place) {
+                    if (!markers.hasOwnProperty(place.id)) {
+                        var marker = new google.maps.Marker({position: new google.maps.LatLng(place.latitude, place.longitude), title: place.name});
+                        marker.setMap(Places.map);
+                        markers[place.id] = 1;
+                        google.maps.event.addListener(marker, 'click', Places.makeInfoWindowForMarker.partial(marker, place));
+                    }
+                });
+            });
     };
-    google.maps.event.addListener(map, 'bounds_changed', $.debounce(2000, showPlacesInMap));
-    showPlacesInMap();
+    google.maps.event.addListener(Places.map, 'bounds_changed', $.debounce(2000, Places.showPlacesInMap));
+    Places.showPlacesInMap();
 
-    $('#new-spot-link').click(function() {
-        $('#new-spot-link').fadeOut(250, function() {
-            $('#new-spot-form').show();
+    // When a marker is clicked, we show a gmaps infowindow with an
+    // update form and a delete 'link'.
+    Places.makeInfoWindowForMarker = function(marker, place) {
+        var updateForm = $('<form/>', {class: 'marker-update-form'});
+        updateForm.append($('<input/>', {type: 'hidden', name: 'id', value: place.id}));
+        updateForm.append($('<input/>', {type: 'text', name: 'name', value: place.name}));
+        updateForm.append($('<input/>', {type: 'text', name: 'longitude', value: place.longitude}));
+        updateForm.append($('<input/>', {type: 'text', name: 'latitude', value: place.latitude}));
+        updateForm.append($('<input/>', {type: 'text', name: 'address', value: place.address}));
+        updateForm.append($('<input/>', {type: 'submit', value: 'Update'}));
+        var deleteLink = $('<span/>', {id: 'delete-link', class: 'fake-link', text: 'delete'});
+        updateForm.append(deleteLink);
+        updateForm.submit(function(e) {
+            e.preventDefault();                                    
+            $.ajax({type: 'POST', url: '/api/update', data: $(e.target).formToJSON()})
+            infoWindow.close();
         });
-    });
+        deleteLink.click(function() {
+            $.ajax({type: 'POST', url: '/api/delete', data: {id: place.id}});
+            infoWindow.close();
+        });
+        var infoWindow = new google.maps.InfoWindow({content: updateForm[0]});
+        infoWindow.open(Places.map, marker);
+    };
 
-    $('#new-spot-form').submit(function(e) {
-        // Don't actually submit this form.
-        e.preventDefault();
-        Places.SubmitFlow.begin();
-    });
-
-    Places = window.Places || {};
-    Places.SubmitFlow = Places.SubmitFlow || {};
+    Places.SubmitFlow = {};
 
     Places.SubmitFlow.begin = function() {
         // Check that they entered something.
@@ -79,7 +76,7 @@
         $('#new-spot-form').fadeOut(250, function() {
             $('#new-spot-status').text('Geocoding..').show().delay(1000).queue(function(next) {
                 // Find the place(s) using Google's geocoder.
-                geocoder.geocode({address: $('#new-spot-where').attr('value')}, function(results, status) {
+                Places.geocoder.geocode({address: $('#new-spot-where').attr('value')}, function(results, status) {
                     if (status === google.maps.GeocoderStatus.OK) {
                         if (results.length > 1) {
                             // Multiple results: let them pick out.
@@ -119,8 +116,8 @@
                     $('#new-spot-status').text('Oops. Come back later?');
                 })
                 .done(function(d) {
-                    showPlacesInMap();
-                    map.setCenter(new google.maps.LatLng(j.latitude, j.longitude));
+                    Places.showPlacesInMap();
+                    Places.map.setCenter(new google.maps.LatLng(j.latitude, j.longitude));
                     $('#new-spot-status').text('"' + j.name + '" has been submitted!').delay(3000).queue(function(next) {
                         $('#new-spot-status').hide();
                         $('#new-spot-link').show();
@@ -132,5 +129,17 @@
             $('#new-spot-form').show();
         }
     };
+
+    $('#new-spot-link').click(function() {
+        $('#new-spot-link').fadeOut(250, function() {
+            $('#new-spot-form').show();
+        });
+    });
+
+    $('#new-spot-form').submit(function(e) {
+        // Don't actually submit this form.
+        e.preventDefault();
+        Places.SubmitFlow.begin();
+    });
 
 })();
